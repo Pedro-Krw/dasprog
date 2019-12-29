74ec9e930e5eb39f40bb4e868e6ea4ec6768cf60
Pedro'Korwa <59316805+korwapedro@users.noreply.github.com>
2019-12-29 17:29:13 +0700

    Add files via upload
    Tugas Praktikum modul 0

---
+++ mencetak ganjil genap.cpp
@@ -0,0 +1,20 @@
+#include "stdio.h"
+#include "windows.h"
+main ()
+{
+	int mace;
+	  system ("color 02");
+	 printf("silahkan maukan nilai  :");
+	  scanf ("%d",&mace);
+	   
+	   if(mace%2==0)
+	   {
+	    system ("color 02");
+	   	printf("\t=>GENAP\n");
+	   	
+	   }
+else
+
+ printf("\t=>GANJIL");
+
+}
