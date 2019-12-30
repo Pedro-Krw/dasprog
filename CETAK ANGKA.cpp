df6e21779305707262bbbae83fc1caaac3f57f39
Pedro'Korwa <59316805+korwapedro@users.noreply.github.com>
2019-12-30 22:56:47 +0700

    Add files via upload
    MENCETAK ANGKA SEDERHANA

---
+++ CETAK ANGKA.cpp
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "windows.h"
+#include "unistd.h"
+
+main (){
+	int sa,ko,de;
+
+	 printf("masuka nilai:");
+	  scanf ("%d",&sa);
+	  usleep(100);
+	   system ("color 57");
+	   for (ko=1;ko<=sa;ko++){
+	   
+	   	
+	   		printf("%3d",ko);
+		   }
+	   	
+			
+		
+	   }
+	
+	
+	
+	
+
